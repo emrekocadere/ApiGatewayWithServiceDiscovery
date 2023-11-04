374b4514504721698c3adc2be17256f659cc60c7
emrekocadere <emrekocadere34@gmail.com>
2023-11-04 22:36:00 +0300

    Add service discovery and Consul integration

--- Plane.API/Program.cs
+++ Plane.API/Program.cs
@@ -1,3 +1,5 @@
+using Car.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.ConfigurationConsul(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +21,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.RegisterWithConsul(app.Lifetime);
 app.MapControllers();
 
 app.Run();
