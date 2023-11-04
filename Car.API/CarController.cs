f46a39e07db7217f6f6812682b994864d2cb907e
emrekocadere <emrekocadere34@gmail.com>
2023-11-04 22:34:19 +0300

    Add CarController class

---
+++ Car.API/CarController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Car.API;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class CarController:ControllerBase
+{
+    [HttpPost]
+    public string  CarApi()
+    {
+        return "Here is Car Controller";
+    }
+}
