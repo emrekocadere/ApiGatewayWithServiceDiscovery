84f7119f820b95206799771bb06405d469101d11
emrekocadere <emrekocadere34@gmail.com>
2023-11-04 22:35:19 +0300

    Add PlaneController class

---
+++ Plane.API/PlaneController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Plane.API;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class PlaneController : ControllerBase
+{
+    [HttpPost]
+    public string PlaneApi()
+    {
+        return "Here is Plane Controller";
+    }
+}
+
+
